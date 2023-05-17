ce728dfb22b8f927814d28b706d38a1ef232b28d
txxasif <txxasif@gmail.com>
2023-05-17 23:10:36 +0600

    third -1 commit

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,6 @@ import { useEffect, useState } from 'react';
 
 
 export default function Home({userPost}) {
-  const [msg,setMsg] = useState('');
-  console.log(userPost);
-  const a =  async() => {
-      let response = await fetch('/api/mongodb');
-      let data = await response.json();
-      console.log(data);
-      setMsg(data);
-  }
-  useEffect(()=>{
-    try{
-       a();
-    }catch(er){
-      console.log(er);
-    }
-  },[])
   return (
     <h1>
     {
