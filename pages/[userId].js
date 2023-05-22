1f4f28dc925120eb9dbd4d194b61d04dda60183c
txxasif <txxasif@gmail.com>
2023-05-22 18:22:47 +0600

    third -1 commit

--- pages/[userId].js
+++ pages/[userId].js
@@ -6,7 +6,8 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import useSWR from 'swr';
-export default function ProfileIndex({userData}){
+export default function ProfileIndex(){
+
     const fetcher = async(url)=>await axios.get(url).then((res)=>res.data.data)
     const check = useSelector(isPostCreatedSelector);
     const router = useRouter();
@@ -25,22 +26,22 @@ export default function ProfileIndex({userData}){
     }
     return(
         <div>
-            <Profile user = {userData} />
+            <Profile user = {data} />
         </div>
     )
 }
 
-export async function getServerSideProps(context){
-    const SERVER_URL = process.env.NODE_ENV === "production" ? "https://your-production-server.com" : "http://localhost:3000";
+// export async function getServerSideProps(context){
+//     const SERVER_URL = process.env.NODE_ENV === "production" ? "https://your-production-server.com" : "http://localhost:3000";
 
-    const { userId } = context.params;
-    console.log(userId)
-    const url = `${SERVER_URL}/api/post/${userId}`;
-    const response = await axios.get(url);
-    console.log(response.data.data,'from server');
-    return {
-        props: {
-           userData: response.data.data
-        }
-    }
-}
+//     const { userId } = context.params;
+//     console.log(userId)
+//     const url = `${SERVER_URL}/api/post/${userId}`;
+//     const response = await axios.get(url);
+//     console.log(response.data.data,'from server');
+//     return {
+//         props: {
+//            userData: response.data.data
+//         }
+//     }
+// }
